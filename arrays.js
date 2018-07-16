7611dbd0f342709d772d620f0227389f9d59a2ca
ALEXANDRA ULVEN <lexieulven@gmail.com>
2018-07-16 20:53:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,7 @@ function removeElementFromBeginningOfArray(array) {
   return s
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
 
